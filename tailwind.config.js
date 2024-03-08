22abf11bd2dbdf16a376468d0610de0e302ef540
ManikandanJagath <sajmani07@gmail.com>
2024-03-08 13:56:33 +0530

    00

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const tailwindConfig = {
   content: [
     "./index.html",
     "./src/main.js",
@@ -8,14 +8,14 @@ export default {
     extend: {
       spacing: {
         '18': '4.5rem', // Equivalent to 18 * 1rem (assuming the default font size is 1rem)
-        '20': '5rem', // Equivalent to 20 * 1rem
+        '20': '5rem',  // Equivalent to 20 * 1rem
         '22': '5.5rem', // Equivalent to 22 * 1rem
-        '24': '6rem', // Equivalent to 24 * 1rem
+        '24': '6rem',  // Equivalent to 24 * 1rem
         '26': '6.5rem', // Equivalent to 26 * 1rem
         '30': '7.5rem', // Equivalent to 30 * 1rem
-        '32': '8rem', // Equivalent to 32 * 1rem
+        '32': '8rem',  // Equivalent to 32 * 1rem
         '34': '8.5rem', // Equivalent to 34 * 1rem
-        '36': '9rem', // Equivalent to 36 * 1rem
+        '36': '9rem',  // Equivalent to 36 * 1rem
         '38': '9.5rem', // Equivalent to 38 * 1rem
         '40': '10rem', // Equivalent to 40 * 1rem
         '50': '12.5rem', // Equivalent to 50 * 1rem
@@ -30,5 +30,6 @@ export default {
     },
   },
   plugins: [],
-}
+};
 
+export default tailwindConfig;
